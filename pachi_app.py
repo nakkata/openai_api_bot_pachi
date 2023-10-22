b1189c2a1ba7feed120aab1bc362e6b2d93f3f2a
t.nakata <109114757+nakkata@users.noreply.github.com>
2023-10-23 08:39:51 +0900

    Update pachi_app.py

--- pachi_app.py
+++ pachi_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import openai
 import secret_keys  # 外部ファイルにAPI keyを保存
 
-openai.api_key = secret_keys.openai_api_key
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 system_prompt = """
 あなたはパチスロ規則を把握した優秀なアシスタントです。
